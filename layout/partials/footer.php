503bd3b156b81463e38b54f0d23ce0d36dcba58a
Arthur Gibbs <Arthur.gibbs@diamond.ac.uk>
2015-05-28 13:02:06 +0100

    remove moodle logo and help link

--- layout/partials/footer.php
+++ layout/partials/footer.php
@@ -47,14 +47,12 @@ if ($hasfooter) { ?>
         <?php if (!empty($PAGE->theme->settings->footnote)) { ?>
             <?php echo $PAGE->theme->settings->footnote; ?>
         <?php }?>
-        <p class="helplink"><?php echo page_doc_link(get_string('moodledocslink')) ?></p>
+
         <?php
         echo $OUTPUT->login_info();
         ?>
         <p>
-            <a href="http://moodle.org" title="Moodle">
-                <img src="<?php echo $OUTPUT->pix_url('moodle-logo','theme')?>" alt="Moodle logo" />
-            </a>
+
         </p>
         <?php echo $OUTPUT->standard_footer_html(); ?>
     </footer>
